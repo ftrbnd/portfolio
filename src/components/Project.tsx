f26f6bae9a7d3010bab854e99abd78216320e196
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-06-06 16:28:04 -0700

    Create projects section

---
+++ src/components/Project.tsx
@@ -0,0 +1,45 @@
+import type { FavoriteProject } from '../projects/favorites';
+import { createResource, type Component } from 'solid-js';
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit({
+	auth: process.env.GITHUB_TOKEN,
+});
+
+const fetchRepository = async (repository_name: string) => {
+	const res = await octokit.request(`GET /repos/{owner}/{repo}`, {
+		owner: 'ftrbnd',
+		repo: repository_name,
+	});
+
+	return res.data;
+};
+
+const Project: Component<FavoriteProject> = (props) => {
+	const [data] = createResource(props.repository_name, fetchRepository);
+
+	return (
+		<div class='card w-96 h-64 bg-base-100 shadow-xl image-full'>
+			<figure>
+				<img
+					src={props.screenshots[0]}
+					alt='Screenshot'
+				/>
+			</figure>
+			<div class='card-body'>
+				<h2 class='card-title'>{props.project_name}</h2>
+				<p>{data()?.description}</p>
+				<div class='card-actions justify-end'>
+					<a
+						class='btn btn-primary'
+						href={data()?.homepage ?? data()?.html_url}
+						target='_blank'>
+						Visit
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Project;
