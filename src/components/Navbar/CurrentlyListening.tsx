f517569880968b1c2d2ed49e48a63241b7cca701
ftrbnd <giosalas25@gmail.com>
2024-10-08 13:29:28 -0700

    Use switch component

--- src/components/Navbar/CurrentlyListening.tsx
+++ src/components/Navbar/CurrentlyListening.tsx
@@ -1,4 +1,4 @@
-import { Match, Show, Switch, createResource, type Component } from 'solid-js';
+import { Match, Switch, createResource, type Component } from 'solid-js';
 import type { Response } from '../../utils/types';
 
 const ROOT_URL = 'https://ws.audioscrobbler.com/2.0/';
@@ -26,60 +26,60 @@ const CurrentlyListening: Component = () => {
 	const derivedClass = () =>
 		track()?.['@attr']?.nowplaying === 'true' ? 'fa-fade' : '';
 
-	const Fallback = () => (
-		<Switch fallback={<p class='text-error'>Not found</p>}>
-			<Match when={track.loading}>
-				<span class='loading loading-dots loading-sm'></span>
-			</Match>
-			<Match when={track.error}>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					class='stroke-current shrink-0 h-6 w-6 text-error'
-					fill='none'
-					viewBox='0 0 24 24'>
-					<path
-						stroke-linecap='round'
-						stroke-linejoin='round'
-						stroke-width='2'
-						d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
-					/>
-				</svg>
-			</Match>
-		</Switch>
+	const Error = () => (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			class='stroke-current shrink-0 h-6 w-6 text-error'
+			fill='none'
+			viewBox='0 0 24 24'>
+			<path
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='2'
+				d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
+			/>
+		</svg>
 	);
 
 	return (
-		<Show
-			when={track()}
-			fallback={<Fallback />}>
-			{(track) => (
-				<div class='card bg-base-100 image-full'>
-					<figure>
-						<img
-							src={track().image[3]['#text']}
-							alt={track().album['#text']}
-							class='w-full max-h-12'
-						/>
-					</figure>
-					<div class='card-body flex items-center justify-center py-0'>
-						<a
-							href={`https://last.fm/user/${USERNAME}`}
-							target='_blank'
-							class='flex flex-col sm:flex-row gap-2 items-center justify-center text-center'>
-							<i class={`fa-solid fa-record-vinyl ${derivedClass()} fa-xl`}></i>
-							<div class='hidden sm:flex flex-col'>
-								<h2 class='card-title hidden sm:block text-start'>
-									{track().name}
-								</h2>
-								<p class='hidden lg:block text-start'>
-									{track().artist['#text']}
-								</p>
-							</div>
-						</a>
+		<Switch fallback={<Error />}>
+			<Match when={track.error}>
+				<Error />
+			</Match>
+			<Match when={track.loading}>
+				<span class='loading loading-dots loading-sm'></span>
+			</Match>
+			<Match when={track()}>
+				{(track) => (
+					<div class='card bg-base-100 image-full'>
+						<figure>
+							<img
+								src={track().image[3]['#text']}
+								alt={track().album['#text']}
+								class='w-full max-h-12'
+							/>
+						</figure>
+						<div class='card-body flex items-center justify-center py-0'>
+							<a
+								href={`https://last.fm/user/${USERNAME}`}
+								target='_blank'
+								class='flex flex-col sm:flex-row gap-2 items-center justify-center text-center'>
+								<i
+									class={`fa-solid fa-record-vinyl ${derivedClass()} fa-xl`}></i>
+								<div class='hidden sm:flex flex-col'>
+									<h2 class='card-title hidden sm:block text-start'>
+										{track().name}
+									</h2>
+									<p class='hidden lg:block text-start'>
+										{track().artist['#text']}
+									</p>
+								</div>
+							</a>
+						</div>
 					</div>
-				</div>
-			)}
-		</Show>
+				)}
+			</Match>
+		</Switch>
 	);
 };
 
