2cadc1a1b2d654b2daf1074a7006cfbbcc279a27
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-12-11 22:38:27 -0800

    Set cache headers and update config

--- astro.config.ts
+++ astro.config.ts
@@ -7,9 +7,5 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), solidJs()],
-	output: 'server',
 	adapter: netlify(),
-	image: {
-		domains: ['cloudfront.net'],
-	},
 });
